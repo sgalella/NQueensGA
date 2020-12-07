eb9088d5b5bb47e21c80d70bd73686e647c8b963
sgalella <santi.galella@gmail.com>
2020-12-07 20:29:14 +0100

    Correct package imports
    Imports are updated with the new package name.

--- tests/test_mutations.py
+++ tests/test_mutations.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-from n_queens.genetic_algorithm import GeneticAlgorithm as ga
+from genetic_nqueens import GeneticAlgorithm as ga
 
 
 class TestMutations(unittest.TestCase):
