5b4a1cbe543f31d0ac0e8902b7d27ce171fd3e56
sgalella <santi.galella@gmail.com>
2020-12-08 13:24:21 +0100

    Make tests a package directory

--- tests/test_recombinations.py
+++ tests/test_recombinations.py
@@ -1,8 +1,7 @@
 import unittest
+
 import numpy as np
-import os
-import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from genetic_nqueens import GeneticAlgorithm as ga
 
 
