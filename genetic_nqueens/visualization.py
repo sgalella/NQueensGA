e23a77eae022f4cc7ba755eec32e11f24ccf303b
sgalella <santi.galella@gmail.com>
2020-12-10 19:41:24 +0100

    Plot incorrect queen positions
    If two queens capture each other, print their positions in red.

--- genetic_nqueens/visualization.py
+++ genetic_nqueens/visualization.py
@@ -6,11 +6,16 @@ def plot_board(solution):
     # Board window
     _, ax = plt.subplots()
     board_size = len(solution)
-    cmap = colors.ListedColormap(['DarkGoldenRod', 'Gold', 'LimeGreen'])
+    cmap = colors.LinearSegmentedColormap.from_list(None, ['DarkGoldenRod', 'Gold', 'LimeGreen', 'Red'])
+    norm = plt.Normalize(0, 3)  # 4 colors to represent the board
     board = [[1 if (row + col) % 2 == 0 else 0 for col in range(board_size)] for row in range(board_size)]
+    wrong_queens = _get_wrong_positions(solution)
     for idx in range(board_size):
-        board[solution[idx]][idx] = 2
-    ax.imshow(board, cmap=cmap)
+        if idx not in wrong_queens:
+            board[solution[idx]][idx] = 2
+        else:
+            board[solution[idx]][idx] = 3
+    ax.imshow(board, cmap=cmap, norm=norm)
     plt.xticks([])
     plt.yticks([])
     plt.savefig('images/board.jpg')
@@ -41,3 +46,24 @@ def plot_diversity(diversity_genotype, diversity_phenotype):
     plt.grid(alpha=0.3)
     plt.savefig('images/diversity.jpg')
     plt.draw()
+
+
+def _get_wrong_positions(solution):
+    """
+    Returns the positions of queens that are incompatible.
+
+    Args:
+        solution (np.array): Row position of each queen in their respective column.
+
+    Returns:
+        set: Wrong queen positions.
+    """
+    wrong = set()
+    for i in range(len(solution)):
+        for j in range(i + 1, len(solution)):
+            if solution[i] == solution[j]:
+                wrong.update([i, j])
+            else:
+                if abs(solution[i] - solution[j]) == abs(i - j):
+                    wrong.update([i, j])
+    return wrong
